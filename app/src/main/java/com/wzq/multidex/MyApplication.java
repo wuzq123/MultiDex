9fb70fdaa723b4756c4f924575634a2b0eee85d2
wuzhiqian <695445360@qq.com>
2017-02-16 16:43:38 +0800

    Initial commit

---
+++ app/src/main/java/com/wzq/multidex/MyApplication.java
@@ -0,0 +1,12 @@
+package com.wzq.multidex;
+
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by wzq on 17-2-16.
+ */
+
+public class MyApplication extends MultiDexApplication{
+
+}
